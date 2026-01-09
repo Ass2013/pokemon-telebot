409448b5fa6116cabeb5135f3fa8fea8a2eb8e6c
Anton <ant.ser.sol@gmail.com>
2026-01-09 16:58:42 +0100

    Implement feeding feature for Pokemon class

--- pokemonproject/M1L4/logic.py
+++ pokemonproject/M1L4/logic.py
@@ -1,6 +1,7 @@
 from random import randint
 import requests
-
+from datetime import datetime
+from datetime import timedelta
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
@@ -13,14 +14,14 @@ class Pokemon:
         self.name = self.get_name()
         self.weight = self.get_weight()
         self.type = self.get_type()
-        self.hp = randint(30,320)
+        self.max_hp = randint(30,320)
+        self.hp = self.max_hp
         self.power = randint(10,300)
+        self.last_feed_time = datetime.now()
 
         Pokemon.pokemons[pokemon_trainer] = self
 
-    # Метод для получения картинки покемона через API
-    
-    # Метод для получения имени покемона через API
+
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         response = requests.get(url)
@@ -49,6 +50,16 @@ class Pokemon:
         else:
             return "electric"
         
+    def feed(self, feed_interval = 40, hp_increase = 30):
+        current_time = datetime.now()  
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Your Pokmeons health increased: current health{self.hp}"
+        else:
+            return f"Next feeding time for your pokemon: {current_time-delta_time}"
+        
     def get_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         response = requests.get(url)
@@ -59,15 +70,17 @@ class Pokemon:
             return "https://wiki.pokemoncentral.it/Pikachu"
 
 
+    def show_img(self):
+        return self.img
     # Метод класса для получения информации
     def info(self):
-        return f"The name of your Pokemon is: {self.name}, The weight of {self.name} is {self.weight}kg. {self.name}'s types are {self.type}, the hp of {self.name} are {self.hp}, the attack of {self.name} is {self.power} "
+        return f"The name of your Pokemon is: {self.name}, The weight of {self.name} is {self.weight}kg. {self.name}'s types are {self.type}, the hp of {self.name} are {self.hp}/{self.max_hp}, the attack of {self.name} is {self.power} "
 
     def attack(self, enemy):
-        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+        if isinstance(enemy, Wizard):  # Проверка на то, что enemy является Wizard
             chance = randint(1,5)
-        if chance == 1:
-            return "Покемон-волшебник применил щит в сражении"
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
         if enemy.hp > self.power:
             enemy.hp -= self.power
             return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
@@ -75,9 +88,13 @@ class Pokemon:
             enemy.hp = 0
             return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
     
-class Wizard(Pokemon):
-    pass
+    # Метод для восстановления здоровья
     
+class Wizard(Pokemon):
+    def feed (self):
+        return super().feed(hp_increase=60)
+
+
 class Fighter(Pokemon):
     def attack(self, enemy):
         super_power = randint(5,15)
@@ -85,11 +102,9 @@ class Fighter(Pokemon):
         result = super().attack(enemy)
         self.power -= super_power
         return result + f"\nYour pokemon used the Super attack that deals:{super_power} "
-       
     
-    # Метод класса для получения картинки покемона
-    def show_img(self):
-        return self.img
+    def feed (self):
+        return super().feed(feed_interval=20)
 
 
 
